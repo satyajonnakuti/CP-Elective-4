230793d4b717da56ea5ca479379390e65ac3f089
satyanarayana reddy jonnakuti <satyajonnakuti@msitprogram.net>
2021-08-31 11:24:23 +0530

    updated

--- 10-issorted-Python/issorted.py
+++ 10-issorted-Python/issorted.py
@@ -6,5 +6,14 @@
 # the list.
 
 def issorted(a):
-	# your code goes here
-	pass
+    count=0
+    for i in range(len(a)):
+        if(i==len(a)-1):
+            break
+        if(a[i]>a[i+1]):
+            count+=1
+    if(count==0):
+        return True
+    elif(count==len(a)-1):
+        return True
+    return False
