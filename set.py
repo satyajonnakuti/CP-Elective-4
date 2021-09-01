a3b0e0ce675faa30b7b78bc87356a23ad59f2cca
satyanarayana reddy jonnakuti <satyajonnakuti@msitprogram.net>
2021-09-01 09:04:40 +0530

    updated

--- set.py
+++ set.py
@@ -8,5 +8,24 @@
 # [ {}, {1}, {2}, {3}, {4}, {5}, {1, 2} ]
 
 def limitedPowerSet(n, k):
-    # Your code goes here...
-    pass
+    list=[]
+    count=0
+    list.append({})
+    count+=1
+    for j in range(1,n+1):
+        #L=set()
+        list.append({j})
+        count+=1
+        if(count>=k):
+                return list
+    
+    for i in range(1,n+1):
+        for j in range(i+1,n+1):
+            list.append({i,j})
+            count+=1
+            if(count>=k):
+                return list
+     
+    
+        
+print(limitedPowerSet(5,10))
